3b88e131f9c7e6f7084fd66bd059a2c0ed81e4a9
Juanjoseve3 <100102850+Juanjoseve3@users.noreply.github.com>
2023-02-11 18:12:55 -0500

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -8,15 +8,13 @@ Original file is located at
 """
 
 import random
-def crear_matriz(n, matriz = []):
-  for i in range(n):
+def crear_matriz(n, matriz = []):  
+  for i in range(n): 
     matriz.append([])
-    for j in range(n):
-      matriz[i].append(random.randint(0,10))
+    for j in range(n): matriz[i].append(random.randint(0,10))
   return matriz
 def mostrar_matriz(matriz):(print(matriz))
 def sumatoria(matriz, c = 0):
-  for i in range(len(matriz)):
-   a = sum(matriz[i]);c += a
+  for i in range(len(matriz)): a = sum(matriz[i]);c += a
   return c
-sumatoria(matrix)
+sumatoria(crear_matriz(3))
